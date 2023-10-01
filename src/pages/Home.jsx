5574a5d9ba993f35dd6cb4ca98c354741e63e1c0
kmseunh <tmdgus8779@gmail.com>
2023-10-01 23:57:28 +0900

    feat: implement Home component and user authentication logic
    Implement the Home component with user authentication logic using AuthContext

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
+import { Link } from 'react-router-dom';
+
 const Home = () => {
-    return <h1>Home</h1>;
+    // AuthContext로부터 사용자 정보와 로그인, 로그아웃 함수 가져오기
+    const { user, loginUser, logoutUser } = useContext(AuthContext);
+
+    return (
+        <div>
+            <h1>퀴즈 게임 앱</h1>
+            <p>다양한 주제와 난이도의 퀴즈를 풀어보세요!</p>
+            {user ? (
+                <div>
+                    <p>안녕하세요, {user.username}님!</p>
+                    <button onClick={logoutUser}>로그아웃</button>
+                    <Link to='/quiz'>
+                        <button>퀴즈 시작</button>
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    <p>로그인이 필요합니다.</p>
+                    <button onClick={loginUser}>로그인</button>
+                </div>
+            )}
+            <Link to='/leaderboard'>랭킹 보기</Link>
+            &nbsp;
+            <Link to='/profile'>프로필 관리</Link>
+        </div>
+    );
 };
 
 export default Home;
