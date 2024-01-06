4f82e2a292f3dd06b426190dca9a59096cf42efd
kmseunh <tmdgus8779@gmail.com>
2024-01-06 16:20:26 +0900

    feat: implement quiz submission

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,63 @@
-import { Link } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import { useState } from 'react';
 
-const Home = () => {
-    // useAuth Hook을 사용하여 사용자 정보와 로그인, 로그아웃 함수 가져오기
-    const { user, loginUser, logoutUser } = useAuth();
+export default function Home() {
+    const [answer, setAnser] = useState('');
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState('typing');
+
+    if (status == 'success') {
+        return <h1>정답입니다!</h1>;
+    }
+
+    function submitForm(answer) {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                let shouldError = answer.toLowerCase() !== 'ronaldo';
+                if (shouldError) {
+                    reject(
+                        new Error('Good guess but a wrong answer. Try again!')
+                    );
+                } else {
+                    resolve();
+                }
+            }, 1500);
+        });
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        setStatus('submitting');
+        try {
+            await submitForm(answer);
+            setStatus('success');
+        } catch (err) {
+            setStatus('typing');
+            setError(err);
+        }
+    }
+
+    function handleTextChange(e) {
+        setAnser(e.target.value);
+    }
 
     return (
-        <div>
-            <h1>퀴즈 게임 앱</h1>
-            <p>다양한 주제와 난이도의 퀴즈를 풀어보세요!</p>
-            {user ? (
-                <div>
-                    <p>안녕하세요, {user.username}님!</p>
-                    <button onClick={logoutUser}>로그아웃</button>
-                    <Link to='/quiz'>
-                        <button>퀴즈 시작</button>
-                    </Link>
-                </div>
-            ) : (
-                <div>
-                    <p>로그인이 필요합니다.</p>
-                    <button onClick={loginUser}>로그인</button>
-                </div>
-            )}
-            <Link to='/leaderboard'>랭킹 보기</Link>
-            &nbsp;
-            <Link to='/profile'>프로필 관리</Link>
-        </div>
+        <>
+            <h2>Soccer Quiz</h2>
+            <p>Who is the best soccer player in the world?</p>
+            <form onSubmit={handleSubmit}>
+                <textarea
+                    value={answer}
+                    onChange={handleTextChange}
+                    disabled={status === 'submitting'}
+                ></textarea>
+                <br />
+                <button
+                    disabled={answer.length === 0 || status === 'submitting'}
+                >
+                    Submit
+                </button>
+                {error !== null && <p className='Error'>{error.message}</p>}
+            </form>
+        </>
     );
-};
-
-export default Home;
+}
